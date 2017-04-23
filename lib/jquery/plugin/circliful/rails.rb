1bfe7d6e1c4398ab17303e6d02a700212657c62f
陳璟皓 <howard60915@gmail.com>
2017-04-23 20:07:55 +0800

    git init

---
+++ lib/jquery/plugin/circliful/rails.rb
@@ -0,0 +1,11 @@
+require "jquery/plugin/circliful/rails/version"
+
+module Jquery
+  module Plugin
+    module Circliful
+      module Rails
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/jquery/plugin/circliful/rails/version.rb
@@ -0,0 +1,9 @@
+module Jquery
+  module Plugin
+    module Circliful
+      module Rails
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
