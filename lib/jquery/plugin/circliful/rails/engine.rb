cf9edf76ccd02b3fd2b45c4a41dc8eda5d8c2bb6
陳璟皓 <howard60915@gmail.com>
2017-05-12 15:12:18 +0800

    move asset to vendor

--- lib/jquery/plugin/circliful/rails/engine.rb
+++ lib/jquery/plugin/circliful/rails/engine.rb
@@ -2,7 +2,7 @@ module Jquery
   module Plugin
     module Circliful
       module Rails
-        class Engine < ::Rails::Engine
+        class Engine < ::Rails::Engine; end
       end
     end
   end
